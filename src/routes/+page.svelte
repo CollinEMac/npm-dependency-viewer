a3121686ea02a6d1be3c538491031a90069cac6e
= <macd.collin@gmail.com>
2024-01-31 00:25:34 -0600

    Display dependency tree

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 // @ts-nocheck
 
   export let form;
+  export let history;
 
   	/** 
      * @param {string} name
@@ -9,6 +10,13 @@
      */
 	async function getSubDeps(name, version) {
     version = version.substring(1); // Remove leading '^'
+
+    // Update history
+    if (history) {
+      history = history + " -> " + name + " @ " + version;
+    } else {
+      history = form.name + " -> " + name + " @ " + version;
+    }
 
     const response = await fetch(
       `https://registry.npmjs.org/${name}/${version}`,
@@ -32,28 +40,45 @@
     <button type="submit">Submit</button>
   </form>
 
+  <div class="history">
+    {#if history}
+      {history}
+    {/if}
+  </div>
+
   <div class="package-name">
     {#if form}
-      <h2>{`${form.name}: `}</h2>
+      {#if form.version}
+        <h2>{`${form.name} @ ${form.version}: `}</h2>
+      {:else}
+        <h2>{`${form.name}: `}</h2>
+      {/if}
       {#if !!form.versions}
         {#each Object.entries(form.versions) as [version, versionObject]}
-          {#if !!versionObject.dependencies}
-            <details>
-              <summary>{version}</summary>
+          <details>
+            <summary>{version}</summary>
+            {#if versionObject.dependencies && Object.keys(versionObject.dependencies).length !== 0}
               {#each Object.entries(versionObject.dependencies) as [dependency, depVersion]}
                 <h3>
                   {dependency}: {depVersion} 
                   <button on:click={getSubDeps(dependency, depVersion)}> Get Sub Dependencies</button>
                 </h3>
               {/each}
-            </details>
-          {/if}
+            {:else}
+              No dependencies
+            {/if}
+          </details>
         {/each}
       {:else}
-        {#if !!form.dependencies}
+        {#if form.dependencies && Object.keys(form.dependencies).length !== 0}
           {#each Object.entries(form.dependencies) as [dependency, depVersion]}
-            <div>{dependency}: {depVersion}</div>
+            <div>
+              {dependency}: {depVersion}
+              <button on:click={getSubDeps(dependency, depVersion)}> Get Sub Dependencies</button>
+            </div>
           {/each}
+        {:else}
+          No dependencies
         {/if}
       {/if}
     {/if}
